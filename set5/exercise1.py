2790edae649707f62e518f51706621ddfb2c97d1
Jacky Tran <jackytranq4@gmail.com>
2022-06-30 17:57:29 +1000

    Set5 Exervise 1

--- set5/exercise1.py
+++ set5/exercise1.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-
+from email import message
 import requests
 
 """REFACTORING
@@ -31,31 +31,43 @@ def wordy_pyramid():
     )
     pyramid_list = []
     for i in range(3, 21, 2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+        list1 = list_of_words_with_lengths(i)
+        for word in list1:
+            pyramid_list.append(word)
     for i in range(20, 3, -2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+        list1 = list_of_words_with_lengths(i)
+        for word in list1:
+            pyramid_list.append(word)
 
     return pyramid_list
 
 
 def get_a_word_of_length_n(length):
-    pass
+    baseURL = (
+        "https://us-central1-waldenpondpress.cloudfunctions.net/"
+        "give_me_a_word?wordlength={i}"
+    )
+    url = baseURL.format(i=length)
+    r = requests.get(url)
+    message = None
+    if r.status_code == 200:
+        message = r.text
+    else:
+        print("failed a request", r.status_code, length)
+    return message
 
 
 def list_of_words_with_lengths(list_of_lengths):
-    pass
+    list_of_words = []
+    if str(list_of_lengths).isdigit():
+        word = get_a_word_of_length_n(list_of_lengths)
+        list_of_words.append(word)
+        return list_of_words
+    if len(list_of_lengths) > 1:
+        for i in list_of_lengths:
+            word = get_a_word_of_length_n(i)
+            list_of_words.append(word)
+        return list_of_words
 
 
 if __name__ == "__main__":
--- set5/readme.md
+++ set5/readme.md
@@ -1,14 +1,8 @@
 TODO: Reflect on what you learned this week and what is still unclear.
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import os
-from datetime import datetime
+baseURL = (
+"https://us-central1-waldenpondpress.cloudfunctions.net/"
+"give_me_a_word?wordlength={length}"
+)
 
-%matplotlib inline
-
-plt.rcParams['figure.figsize'] = (10, 5)
-
-saved_style_state = matplotlib.rcParams.copy() #give us a style state to go back to
+url = baseURL.format(length=i)
